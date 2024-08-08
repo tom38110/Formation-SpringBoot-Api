d7ae1d75b1a8c1786271e1c7665124eb8a539729
tom38110 <75202875+tom38110@users.noreply.github.com>
2024-08-08 16:31:11 +0200

    Add files via upload

---
+++ api/src/main/java/com/openclassrooms/api/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.api.repository;
+
+import com.openclassrooms.api.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
---
+++ api/src/test/java/com/openclassrooms/api/controller/EmployeeControllerTest.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.api.controller;
+
+import com.openclassrooms.api.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].firstName").value("Laurent"));
+    }
+}
